9aee67055d20b10c14a5ed73cc4bafea6dd94858
Arsenii01 <arsa1200@yandex.ru>
2022-11-12 18:16:56 +0300

    First commit

---
+++ src/main/java/ru/practice32/practice3132/Practice3132Application.java
@@ -0,0 +1,13 @@
+package ru.practice32.practice3132;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practice3132Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practice3132Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/practice32/practice3132/Practice3132ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.practice32.practice3132;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practice3132ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
